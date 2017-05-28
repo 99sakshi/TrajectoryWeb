5638d8d32bcfc28bb8f67be6294f304076c01b2f
Naman Gupta <Oponop.tech@gmail.com>
2017-05-28 15:43:44 +0530

    added whitespace

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ console.log('Starting the main Trajectory Server ...');
 
 app.get("/start",function(request, response){
     var id = request.params.id;
-    var obj = { id : id, Content : "content " +id , data : " Simulation Start Karo !!!" };
+    var obj = { id : id, Content : "content " + id , data : " Simulation Start Karo !!!" };
     console.log('Button on front end clicked!');
     response.json(obj);
 });
 
 app.use(express.static('.'));
 var server = app.listen(3000);
-
