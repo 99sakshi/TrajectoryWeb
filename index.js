4ff18a29bc6c33a9900914a081fb7d1b476cc45a
Naman Gupta <Oponop.tech@gmail.com>
2017-05-31 13:28:44 +0530

    Moved server code to Trajectory Server

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 var express = require("express");
-var app = express()
+var app = express();
 
-console.log('Starting the main Trajectory Server ...');
-
-app.get("/start",function(request, response){
-    var id = request.params.id;
-    var obj = { id : id, Content : "content " + id , data : " Simulation Start Karo !!!" };
-    console.log('Button on front end clicked!');
-    response.json(obj);
-});
+console.log('Starting Trajectory Web Server ...');
 
 app.use(express.static('.'));
 var server = app.listen(3000);
