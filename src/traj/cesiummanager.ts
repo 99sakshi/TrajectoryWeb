df46241884e5fdaac7cf1962f8adcf0bf3425fcb
Samriddhi <samriddhi.oponop@gmail.com>
2017-06-08 04:16:18 -0700

    added gulpfile

--- src/traj/cesiummanager.ts
+++ src/traj/cesiummanager.ts
@@ -1,13 +1,23 @@
 declare var Cesium: any;
 import { Injectable } from '@angular/core'
 import { LoadConfig } from './loadconfig.service';
-
+ /**
+ * @ngdoc method
+ * @name @Injectable#decorator
+ */
 
 @Injectable()
 export class CesiumManager{ 
       
       private _cesiumViewer;
       private _config;
+      /**
+ * @ngdoc method
+ * @name Constructor#initializing the current object to required configurations
+ *
+ * @param {loadConfig} event Private variable,receives LoadConfig
+ * Initializing the current object to required configurations
+ */
 
       constructor ( private loadConfig: LoadConfig) {
 
@@ -16,7 +26,11 @@ export class CesiumManager{
                                                             this.init(); 
                                                   } );  
       }
-
+/**
+ * @ngdoc method
+ * @name init#access DEM data from geoserver
+ *
+ */
     
      private init () {
         var viewer =  new Cesium.Viewer('cesiumContainer');
@@ -46,12 +60,26 @@ export class CesiumManager{
 
         this._cesiumViewer = viewer;
       }
-
+/**
+ * @ngdoc method
+ * @name addEntity#adds an entity
+ *
+ * @param {entity} event Receive the emitted entity
+ * Adds the new entity
+ *
+ * @return {retEntity} retEntity returns the retEntity variable and a promise
+ */
       addEntity(entity) {
         var retEntity = this._cesiumViewer.entities.add( entity.getPara());
         return retEntity;
       }
-
+/**
+ * @ngdoc method
+ * @name removeEntity#removes an entity
+ *
+ * @param {entity} event Receive the emitted entity
+ * Removes an entity
+ */
       removeEntity(entity) {
 
       }
