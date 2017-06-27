718be243882a0cd69be7f349691a7602799ae744
Samriddhi <samriddhi.oponop@gmail.com>
2017-06-27 16:58:20 -0700

    tried fetching extents coordinates

--- src/traj/cesiummanager.ts
+++ src/traj/cesiummanager.ts
@@ -28,12 +28,31 @@ export class CesiumManager{
                                                             this.init(); 
                                                   } );  
 
-           this._mouseEndCallback = function() {
-                var extents = this._cesiumViewer.camera.computeViewRectangle();
-                console.log(extents);
+          this._mouseEndCallback = function() {
+                var extents = this._cesiumViewer.camera.computeViewRectangle()
+                console.log(extents)
+                console.log("extents.x");
+                // link: https://cesiumjs.org/Cesium/Build/Documentation/Camera.html,https://cesiumjs.org/Cesium/Build/Documentation/Cartesian3.html
 
                 //TODO: iterate over rectangle and get the entities at each points after 0.1 degree change
            };
+       /*    function getExtentView() {
+    var cl2 = new Cesium.Cartesian2(0, 0);
+    var leftTop = viewer.scene.camera.pickEllipsoid(cl2, ellipsoid);
+
+    cr2 = new Cesium.Cartesian2(viewer.scene.canvas.width, viewer.scene.canvas.height);
+    var rightDown = viewer.scene.camera.pickEllipsoid(cr2, ellipsoid);
+
+    if (leftTop != null && rightDown != null) {
+        leftTop = ellipsoid.cartesianToCartographic(leftTop);
+        rightDown = ellipsoid.cartesianToCartographic(rightDown);
+        return new Cesium.Rectangle(leftTop.longitude, rightDown.latitude, rightDown.longitude, leftTop.latitude);
+    } else {//The sky is visible in 3D
+        console.log("Sky is visible");
+        return null;
+    }
+}*/
+
       }
 
 
@@ -45,7 +64,7 @@ export class CesiumManager{
       private init () {
 
         // show India when app fires up
-        var rectangle = Cesium.Rectangle.fromDegrees(68.0, 7.0, 89.0, 35.0);
+        var rectangle = Cesium.Rectangle.fromDegrees(68.0, 7.0, 89.0, 35.0);//west,south,east,north
         Cesium.Camera.DEFAULT_VIEW_FACTOR = 1.0;
         Cesium.Camera.DEFAULT_VIEW_RECTANGLE = rectangle;
 
